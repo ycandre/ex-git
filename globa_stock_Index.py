0bff80c1c7de73670281a93ff692246ba3f94474
yc.andrewl <yc.andrewl@gmail.com>
2021-10-27 17:26:16 +0800

    modify

--- globa_stock_Index.py
+++ globa_stock_Index.py
@@ -15,8 +15,13 @@ from fake_useragent import UserAgent
 from requests.exceptions import HTTPError
 from io import StringIO
 
+def set_header_ua():
+    user_agent = UserAgent()
+    return user_agent.random
+
+
 def monthly_report(year, month):
-    
+    print ("monthly_report()")
     # 假如是西元，轉成民國
     if year > 1990:
         year -= 1911
@@ -59,38 +64,41 @@ def monthly_report(year, month):
 
 
 def time_Mod(timeString):
-
-    # 輸入格式: 2021-10-26 08:00:00" >>> 輸出格式: 1635206400
-    # timeString = "2021-10-26 08:00:00"
+    #print ("time_Mod()")
+    """ 
+    "輸入格式: 2021-10-26 08:00:00" >>> 輸出格式: 1635206400
+    """
     struct_time = time.strptime(timeString, "%Y-%m-%d %H:%M:%S") # 轉成時間元組
     time_stamp = int(time.mktime(struct_time)) # 轉成時間戳
-    print(time_stamp)    
-    return 0 
+    return time_stamp 
 
-#print (time_Mod("2021-10-26 08:00:00"))
-
+    """ 
+    print (time_Mod("2021-10-22 08:00:00"))
+    print (time_Mod("2021-10-26 08:00:00"))
+    """
 
 def stock_period_report(stock_code, stock_area, start_time, end_time):
+    print ("stock_period_report()")
     """
-    start_time = 946656000  # 2000/1/1
-    end_time = 1600272000  # 2020/9/17
-    stock_code = 2317
-    stock_area = "TW"
+    print ("stock_code", stock_code)
+    print ("stock_area", stock_area)
+    print ("start_time", start_time)
+    print ("end_time"  , end_time)
     """
-
-    url_template= f"https://query1.finance.yahoo.com/v8/finance/chart/{stock_code}.{stock_area}?period1={start_time}&period2={end_time}&interval=1d&events=history&=hP2rOschxO0"
-    print(url_template)
-    print('')
-    #url_template = "https://query1.finance.yahoo.com/v7/finance/download/2330.TW?period1=1633392000&period2=1635206400&interval=1d&events=history&includeAdjustedClose=true"
-    ua = UserAgent()
-    user_agent = ua.random
+    url = f"https://query1.finance.yahoo.com/v8/finance/chart/{stock_code}.{stock_area}?period1={start_time}&period2={end_time}&interval=1d&events=history&=hP2rOschxO0"
     
-    # ua 產生 headers
-    headers = {'user-agent': user_agent}
-        
-    response = requests.get(url_template, headers=headers)
+    # set_header_ua 產生 headers
+    headers = {'user-agent': set_header_ua()}
+    response = requests.get(url, headers=headers)
+
+    print("response", response)
+    print('')
     # 序列化資料回報
     data = json.loads(response.text)
+    print("data", data)
+    print('')
+    
+    
     # 把json格式資料放入pandas中
     df = pd.DataFrame(
         data["chart"]["result"][0]["indicators"]["quote"][0],
@@ -100,7 +108,7 @@ def stock_period_report(stock_code, stock_area, start_time, end_time):
         columns=["open","high","low","close","volume"]
     )
     
-    print (data)
+   # print (data)
     # 印出前3行
     print(df[:3])
     """
@@ -120,7 +128,6 @@ def stock_period_report(stock_code, stock_area, start_time, end_time):
     time.sleep(5)
     return 0
 
-#print (stock_period_report(2330, "TW", time_Mod("2021-10-12 08:00:00"), time_Mod("2021-10-26 08:00:00")))
-print (stock_period_report(2330, "TW", 1633996800, 1635206400))
+print (stock_period_report(2330, "TW", time_Mod("2021-10-12 13:30:00"), time_Mod("2021-10-26 08:00:00")))
     
 
